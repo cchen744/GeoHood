2e53b39823420222ce7ea4622b79b78ee67d4ee6
陈晨萌萌 <chenchenmengmeng@Mackencheese.local>
2026-02-12 22:36:21 -0600

    Drafted main functions

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 Purpose: Convert address to zipcode, then use it to extract housing information
 
 '''
-
 import json
 import argparse
 import requests
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+import geopy
 # import more if you need more packages
 
-API_KEY = '...' # input your geocoding api key
+API_KEY = 'AIzaSyBuNHyspdHBMXTf4usN-WXnZ3RxcK_RarU' # input your geocoding api key
 
 class WebScraper():
     def __init__(self):
@@ -28,7 +28,9 @@ class WebScraper():
         # Get the coordinates from the response
         data = response.text
         # Extract zipcode and coordinate from json file
-        ...
+        data = json.loads(data)
+        zipcode = data['results'][0]['address_components'][-1]['long_name']
+        coordinate = data['results'][0]['geometry']['location']
         return zipcode, coordinate
 
         # Input the zipcode of the target region
@@ -49,32 +51,42 @@ class WebScraper():
         for house in all_houses:
             house = House()
             house.get_info()  # Get house information
-            all_houses.append(house)
-        ...
+            self.house_collection.append(house)
+        
 
 
     # Save house information to csv and photos to a folder
     def save_houses(self, zipcode):
-        ...
-        for house in self.house_collection():  # Iterate all houses in the house collection
+        path = f"houses_{zipcode}"  # Create a folder to save house photos
+        df = pd.DataFrame([house.get_info() for house in self.house_collection])
+        df.to_csv(f"properties_{zipcode}.csv", index=False)
+        for house in self.house_collection:  # Iterate all houses in the house collection
             house.save_file()  # Save house information as file
             house.save_img(path)  # Save house photos to the path
-        ...
+        
 
 
 class House():
     def __init__(self, price, house_address, coordinate, image_url): # information we want to get
         # Set house attributes
         self.price = price
-        ...
+        self.house_address = house_address
+        self.coordinate = coordinate
+        self.image_url = image_url
 
     def get_info(self):
-        house_info =  # Collect a set of attributes of houses
+        house_info = {
+            "price": self.price,
+            "address": self.house_address,
+            "coordinate": self.coordinate,
+            "image_url": self.image_url
+        }
         return house_info
 
     def save_img(self, path):  # Save house images to the path
         img = requests.get(self.image_url).content
-        ...
+        with open(f"{path}/{self.house_address}.jpg", 'wb') as f:
+            f.write(img)
 
 
 
